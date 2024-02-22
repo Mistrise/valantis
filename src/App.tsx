ef18a8fb6a2729902f53fe051e1e3b0f837559d8
Alex <alekseiivanov.2016@inbox.ru>
2024-02-22 17:40:39 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
+interface DataFromApi {
+  action: string,
+  result: string[]
+}
+
+//@ts-nocheck
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect( () => {
+    const currentDate = new Date()
+    // @ts-ignore
+    fetch('http://api.valantis.store:40000/', {
+      method: 'POST',
+      headers: {
+        "X-Auth": `md5("password_${currentDate.getFullYear()}${currentDate.getMonth()}${currentDate.getDate()}")`
+      },
+      body: {
+        // @ts-ignore
+        "action": "get_ids",
+        "params": {"offset": 10, "limit": 3}
+      }
+    })
+        .then(res => res.json())
+        // @ts-ignore
+        .then((data: DataFromApi) => setProducts([...products, data.result]))
+  }, [products]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map(e => {
+        return <div key={e}>{e}</div>
+      })}
     </div>
   );
 }
