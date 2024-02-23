0e65712ed8d45803c6476ee06e2a7ccee8fc0266
aleksejivanov <alekseiivanov.2016@mail.ru>
2024-02-23 16:31:23 +0300

    connect to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,31 @@
 import React, {useEffect, useState} from 'react';
+import authKey from "./helpers/authGenerator";
 
-interface DataFromApi {
-  action: string,
-  result: string[]
-}
-
-//@ts-nocheck
 function App() {
   const [products, setProducts] = useState([])
+
+  const body = {
+    "action": "filter",
+    "params": {"price": 17500.0}
+  }
+
   useEffect( () => {
-    const currentDate = new Date()
-    // @ts-ignore
+
     fetch('http://api.valantis.store:40000/', {
       method: 'POST',
       headers: {
-        "X-Auth": `md5("password_${currentDate.getFullYear()}${currentDate.getMonth()}${currentDate.getDate()}")`
+        "X-Auth": authKey,
+        "Content-Type": "application/json"
       },
-      body: {
-        // @ts-ignore
-        "action": "get_ids",
-        "params": {"offset": 10, "limit": 3}
-      }
+      body: JSON.stringify(body)
     })
         .then(res => res.json())
         // @ts-ignore
-        .then((data: DataFromApi) => setProducts([...products, data.result]))
-  }, [products]);
+        .then((data) => setProducts([...products, data.result]))
+  }, []);
   return (
     <div>
-      {products.map(e => {
-        return <div key={e}>{e}</div>
-      })}
+      {products}
     </div>
   );
 }
---
+++ src/helpers/authGenerator.ts
@@ -0,0 +1,14 @@
+import {md5} from "js-md5";
+
+const date = new Date()
+
+const year = date.getFullYear()
+
+const month = date.getMonth() + 1
+
+const day = date.getDate()
+
+const authKey = md5(`Valantis_${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`)
+
+console.log(`Valantis_${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`)
+export default authKey
