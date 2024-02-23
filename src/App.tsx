370c1042c9c8da4e0d618da1c41f937472b8efde
aleksejivanov <alekseiivanov.2016@mail.ru>
2024-02-23 17:16:39 +0300

    showing response on cite

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import authKey from "./helpers/authGenerator";
 
-function App() {
-  const [products, setProducts] = useState([])
+interface Product {
+  brand: string | null
+  id: string
+  price: number
+  product: string
+}
 
-  const body = {
-    "action": "filter",
-    "params": {"price": 17500.0}
+function App() {
+  const [ids, setIds] = useState<string[]>([])
+
+  const [products, setProducts] = useState<Product[]>([
+    {
+      brand: null,
+      id: 'init',
+      price: 0,
+      product: '',
+    }
+  ])
+
+  const bodyIds = {
+    "action": "get_ids",
+    "params": {"limit": 50}
   }
 
   useEffect( () => {
+    fetch('http://api.valantis.store:40000/', {
+      method: 'POST',
+      headers: {
+        "X-Auth": authKey,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(bodyIds)
+    })
+        .then(res => res.json())
+        .then((data) => setIds([data.result]))
+        .catch(err => console.error(err))
+  }, []);
+
+  useEffect(() => {
 
     fetch('http://api.valantis.store:40000/', {
       method: 'POST',
@@ -17,15 +47,22 @@ function App() {
         "X-Auth": authKey,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify({
+        "action": "get_items",
+        "params": {"ids": ids[0]}
+      })
     })
         .then(res => res.json())
-        // @ts-ignore
-        .then((data) => setProducts([...products, data.result]))
-  }, []);
+        .then((data) => setProducts([...data.result]))
+        .catch(err => console.error(err))
+  }, [ids])
   return (
     <div>
-      {products}
+      {products.map(e => {
+        return <div key={e.id}>
+          {e.product}
+        </div>
+      })}
     </div>
   );
 }
