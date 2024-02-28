66e6e34497315ec46e401530b481fea2b803a449
aleksejivanov <alekseiivanov.2016@mail.ru>
2024-02-28 23:54:46 +0300

    cleanup

--- src/components/NavBar/DenseBar.tsx
+++ src/components/NavBar/DenseBar.tsx
@@ -1,6 +1,6 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import {Button, FormControl, FormGroup, TextField} from "@mui/material";
+import {Button, FormControl, FormGroup, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material";
 import React, {useState} from "react";
 import {fetchFilteredIds} from "../../helpers/api";
 
@@ -12,29 +12,29 @@ export default function DenseBar({setFormInput}: DensProps) {
     const [select, setSelect] = useState('')
     const [textInput, setTextInput] = useState('')
 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (select === "price") {
+            const data = await fetchFilteredIds({[select]: parseInt(textInput, 10)})
+            setFormInput(data)
+        } else {
+            const data = await fetchFilteredIds({[select]: textInput})
+            setFormInput(data)
+        }
+
+    }
+
     return (
         <Box sx={{ flexGrow: 1, marginBottom: 2 }}>
             <AppBar position="static">
-                <form onSubmit={async (event) => {
-                    event.preventDefault()
-                    if (select === "price") {
-                        const data = await fetchFilteredIds({[select]: parseInt(textInput, 10)})
-                        setFormInput(data)
-                    } else {
-                        const data = await fetchFilteredIds({[select]: textInput})
-                        setFormInput(data)
-                    }
-
-                }}>
+                <form onSubmit={(event) => handleSubmit(event)}>
                     <FormGroup>
                         <FormControl>
-                            <select name="filter" id="filter" onChange={(event) => {
-                                setSelect(event.target.value)
-                            }}>
-                                <option value="brand">brand</option>
-                                <option value="price">price</option>
-                                <option value="product">product</option>
-                            </select>
+                            <Select defaultValue="brand" id="filter" onChange={(event:SelectChangeEvent<string>) => setSelect(event.target.value)}>
+                                <MenuItem value="brand">brand</MenuItem>
+                                <MenuItem value="price">price</MenuItem>
+                                <MenuItem value="product">product</MenuItem>
+                            </Select>
                             <TextField placeholder={'Filter'} onChange={(event) => setTextInput(event.target.value)}/>
                             <Button color={"secondary"} type={"submit"}>Submit</Button>
                         </FormControl>
